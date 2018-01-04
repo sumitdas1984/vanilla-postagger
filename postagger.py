2a279f2429ecf035248728c203a1e38b338d5329
administrator <sumit.jucse@gmail.com>
2018-01-04 12:20:51 +0530

    first commit

---
+++ postagger.py
@@ -0,0 +1,13 @@
+from nltk import word_tokenize
+import pickle
+from postagger_train import features
+
+# load the model from disk
+clf = pickle.load(open('vanilla_postagger_model.sav', 'rb'))
+
+def pos_tag(sentence):
+    tagged_sentence = []
+    tags = clf.predict([features(sentence, index) for index in range(len(sentence))])
+    return zip(sentence, tags)
+ 
+print pos_tag(word_tokenize('This is my friend, John.'))
